be5b6b12b8e1505dc65be8f4ab7d1a49c7d28626
Joseph Wangemann <fractalscape13@gmail.com>
2020-03-27 10:41:59 -0700

    add migration to change treat.cs property name

---
+++ PierresTreats/Migrations/20200327174051_TreatUpdate.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PierresTreats.Migrations
+{
+    public partial class TreatUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Treats",
+                newName: "TreatName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TreatName",
+                table: "Treats",
+                newName: "Description");
+        }
+    }
+}
