d498adc8630c54c9396b9d32e68c9362ff902030
Joseph Wangemann <fractalscape13@gmail.com>
2020-03-27 08:48:18 -0700

    create structure for view files

---
+++ PierresTreats/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+  
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using PierresTreats.Models;
+using System.Threading.Tasks;
+using PierresTreats.ViewModels;
+
+namespace PierresTreats.Controllers
+{
+  public class AccountController : Controller
+  {
+    
+  }
+}
---
+++ PierresTreats/Controllers/FlavorsController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using PierresTreats.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace PierresTreats.Controllers
+{
+  public class FlavorsController : Controller
+  {
+
+  }
+}
